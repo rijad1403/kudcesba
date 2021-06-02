56bb1300722c9c613087ca13bacdc5508d14ab9a
Rijad Gračanin <rijad.gracanin14@gmail.com>
2021-06-02 13:38:43 +0200

    made changes to footer and homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  helpTexts = [];
+  reasonsTexts = [];
 
-  constructor() { }
+  constructor() {}
 
+  ngOnInit() {
+    this.helpTexts = [
+      {
+        heading: 'Ideš nekamo?',
+        body: 'Brzo ćeš pronaći ljude koji prolaze kraj tebe i putuju u tvom pravcu zato što imaš pristup milijunima putovanja.',
+      },
+      {
+        heading: 'Poveži se s drugima',
+        body: '  Upiši svoju točnu adresu kako bi pronašao savršen prijevoz. Odaberi s kim želiš putovati. I, rezerviraj!',
+      },
+      {
+        heading: 'Pokret',
+        body: 'Dođi točno tamo gdje želiš. Bez gnjavaže, bez redova i bez čekanja.',
+      },
+    ];
+
+    this.reasonsTexts = [
+      {
+        heading: 'Ti biraš',
+        body: 'Tisuće odredišta po odličnim cijenama.',
+        icon: 'trail-sign-outline',
+      },
+      {
+        heading: 'Sigurno dijeljenje prijevoza',
+        body: 'Potvrda identiteta još je jedan postupak koji čini profile članova sigurnijima.',
+        icon: 'document-lock-outline',
+      },
+      {
+        heading: 'Ne gubi vrijeme',
+        body: 'Ne trebaš ići na drugi kraj grada. Nađi prijevoz koji polazi u tvojoj blizini.',
+        icon: 'location-outline',
+      },
+    ];
+  }
 }
--- src/app/partials/footer/footer.component.ts
+++ src/app/partials/footer/footer.component.ts
@@ -6,9 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  footerPages = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.footerPages = [
+      {
+        header: 'Kudćeš.ba',
+        pages: [
+          {
+            title: 'O nama',
+            link: 'o-nama',
+          },
+          {
+            title: 'FAQ',
+            link: 'faq',
+          },
+          {
+            title: 'Kontakt',
+            link: 'contact',
+          },
+        ],
+      },
+      {
+        header: 'Sigurnost',
+        pages: [
+          {
+            title: 'Privatnost podataka',
+            link: 'privacy-policy',
+          },
+          {
+            title: 'Tačnost podataka',
+            link: 'data-validation',
+          },
+        ],
+      },
+      {
+        header: 'Podrška korisnicima',
+        pages: [
+          {
+            title: 'Kako postaviti oglas',
+            link: 'new-advertisement-help',
+          },
+          {
+            title: 'Kako brzo pronaći vožnju',
+            link: 'find-drive-help',
+          },
+          {
+            title: 'Blog',
+            link: 'blog',
+          },
+        ],
+      },
+    ];
+  }
 }
